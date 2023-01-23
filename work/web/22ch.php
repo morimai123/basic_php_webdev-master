24be0441f4981087bbf1085a8e539e75f7470212
Mai Mori <sghxp267@yahoo.co.jp>
2023-01-23 17:59:13 +0900

    2023012302

--- work/web/22ch.php
+++ work/web/22ch.php
@@ -14,35 +14,41 @@
     <?php
     require_once('Post.php');
 
-    function readPostsFromJson()
-    {
-        //jsonファイルを開く
-        $data_file_name = "data.json";
-        $data = file_get_contents($data_file_name);
-        // data.jsonを配列で読み込み
-        $json = json_decode($data, true);
-        return $json;
-    }
-
+    // jsonファイルからPostクラスの配列を取得する
     $posts = readPostsFromJson();
+
     // 画面から入力した値を変数に入れる
     $newpost = $_GET["newpost"];
 
-    if (isset($_GET["newpost"])) {
-        // 現在日時を取得
-        $newdate = new DateTime('now');
-        $newdate = $newdate->format('Y-m-d H:i');
-        // 入力された値と現在日時を配列に入れる
-        $array["date"] = $newdate;
-        $array["post"] = $newpost;
-        array_push($posts, $array);
-        // 画面から入力した値をdata.jsonを配列で書き込み
-        $newposts = json_encode($posts, JSON_UNESCAPED_UNICODE);
-        file_put_contents("data.json", $newposts);
+    // 現在日時を取得
+    $newdate = new DateTime('now');
+    $newdate = $newdate->format('Y-m-d H:i');
+
+    // 入力された投稿と現在日時でPostクラスを作る
+    $p = new Post($newdate, $newpost);
+    array_push($posts, $p);
+    // print_r($posts);
+    // 新配列の要素を取り出す
+    foreach ($posts as $i) {
+        // オブジェクトを配列化
+        $post = (array)$i;
+        // print_r($post);
+        $post2 = (array)$post;
+        print_r($post2);
+
     }
 
+    // keyを元に戻す方法が分からず(Postdttm、Postpost) 、、配列の中に配列を入れる方法も分からない、、    
+
+
+    // 画面から入力した値をdata.jsonを配列で書き込み
+    // $newposts = json_encode($posts, JSON_UNESCAPED_UNICODE);
+    // file_put_contents("data.json", $newposts);
+
+
+    // それぞれの投稿を取り出す
     foreach ($posts as $post) {
-        $post = new Post($post['date'], $post['post']);
+        // $post = new Post($post['date'], $post['post']);
         // $post->createNewPost($newpost);
         //Postクラスのget関数を使って表示
         echo "<div class ='card'>";
@@ -56,7 +62,33 @@
     //   echo "<div class ='post'>" . $posts[$i]->post . "</br></div>";
     //   echo "</div>";
     // }
+
+    // data.jsonを読み込んで配列化
+    function readPostsFromJson()
+    {
+        //data.jsonを開く
+        $data_file_name = "data.json";
+        $data = file_get_contents($data_file_name);
+        // data.jsonを配列で読み込み
+        $json = json_decode($data, true);
+        print_r($json);
+        echo "<br/>";
+        echo "<br/>";
+        $arr = array();
+        foreach ($json as $j) {
+            $post = new Post($j['date'], $j['post']);
+            // print_r($post);
+            // echo "<br/>";
+            // echo "<br/>";
+            array_push($arr, $post);
+        }
+        //  print_r($arr);
+        return $arr;
+    }
+
     ?>
 </body>
+
+</html>
 
 </html>
